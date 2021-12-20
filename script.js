0fe20166ff77211f10061a9339346c36de114ce2
Delphine <delphine56.dent@hotmail.fr>
2021-12-20 11:19:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let cardContainers = document.querySelectorAll('.cardContainer');
+let derriere = document.querySelectorAll('.derriere')
+
+ function fermer(e){
+    //derriere.forEach(e => e.classList.toggle('rotateDerriere'));
+    //console.log(this.childNodes);
+    this.childNodes[3].classList.toggle('rotateDerriere')
+   }
+    
+ 
+
+ cardContainers.forEach(cardContainer => cardContainer.addEventListener('click',fermer));
+
+
+ 
